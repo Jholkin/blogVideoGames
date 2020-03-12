ee22f588d70d33b67b1dd777a930ee45e3ed9b31
jholkin <jhil.palacios@unas.edu.pe>
2020-03-12 18:48:18 -0500

    Blog de videojuegos hecho en php con mysql

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+    <div class="clearfix"></div>
+</div>
+<!-- Fin del container -->
+    <!-- Pie de página -->
+        <footer id="footer">
+            <p>Desarrollado por Jhil Palacios &copy; 2019</p>
+        </footer>
+    </body>
+</html>
